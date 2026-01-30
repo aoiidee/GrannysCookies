128ad9ceff802c66171cd5c5fc4078e6ffcb2d73
PierceNunn <agentp5151@gmail.com>
2026-01-30 15:48:12 -0600

    offset dragged objects

--- Assets/Scripts/DraggableUIElement.cs
+++ Assets/Scripts/DraggableUIElement.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 public class DraggableUIElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Canvas parentCanvas;
+    private Vector2 offset;
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
         //throw new System.NotImplementedException();
+        offset = (Vector2)gameObject.GetComponent<RectTransform>().position - eventData.position;
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
@@ -23,7 +25,7 @@ public class DraggableUIElement : MonoBehaviour, IBeginDragHandler, IDragHandler
 
     private void SetDraggedPosition(PointerEventData eventData)
     {
-        gameObject.GetComponent<RectTransform>().position = eventData.position;
+        gameObject.GetComponent<RectTransform>().position = eventData.position + offset;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
