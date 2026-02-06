b7e62464bddbeec152f045664324e907ce5ac1bc
PierceNunn <agentp5151@gmail.com>
2026-02-06 15:28:55 -0600

    converted to use prefabs instead of scenes

--- Assets/Scripts/PopupFunctions.cs
+++ Assets/Scripts/PopupFunctions.cs
@@ -7,20 +7,29 @@
 *****************************************************************************/
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using UnityEngine.Rendering.Universal;
 
 public class PopupFunctions : MonoBehaviour
 {
+    [SerializeField] private GameObject _testPrefab;
     [SerializeField] private int testScene;
     [SerializeField] private RenderTexture rt;
+    [SerializeField] private int _targetPopupHeight = 5;
+
+    private List<int> loadedPopupIDs = new List<int>();
 
     private RectTransform targetPopup;
+
+    [SerializeField] private float _clearance = 50;
 
     public RectTransform TargetPopup { get => targetPopup; set => targetPopup = value; }
 
     public async void TestLoadScene()
     {
-        await SceneManager.LoadSceneAsync(testScene, LoadSceneMode.Additive);
+        GameObject g = Instantiate(_testPrefab, Vector2.down * (_clearance * loadedPopupIDs.Count), Quaternion.identity);
+        loadedPopupIDs.Add(g.GetInstanceID());
+        /*await SceneManager.LoadSceneAsync(testScene, LoadSceneMode.Additive);
         Scene s = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
         print(s.name);
         Camera[] test = GameObject.FindObjectsByType<Camera>(FindObjectsSortMode.None);
@@ -30,12 +39,14 @@ public class PopupFunctions : MonoBehaviour
             {
                 //c.targetTexture = rt;
             }
-        }
+        }*/
     }
 
     public void SetTargetPopup(RectTransform target)
     {
+        //targetPopup.GetComponentInParent<Canvas>().sortingOrder = 0;
         targetPopup = target;
+        //targetPopup.GetComponentInParent<Canvas>().sortingOrder = _targetPopupHeight;
     }
 
     public void KillScene(GameObject s)
--- Assets/Scripts/RenderTextureMaker.cs
+++ Assets/Scripts/RenderTextureMaker.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RenderTextureMaker : MonoBehaviour
 {
+    [SerializeField] private Camera targetCam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RenderTexture r = new RenderTexture(256, 256, 1);
+        targetCam.targetTexture = r;
+        gameObject.GetComponent<RawImage>().texture = r;
     }
 
     // Update is called once per frame
