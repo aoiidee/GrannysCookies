bf47eeacdef7a9ee1c551b8059dd16ae39db8b06
Gabe355 <gandrews1@mail.bradley.edu>
2026-02-05 19:22:49 -0600

    Block and YesOrNo Captcha

---
+++ Assets/ScriptsTemp/BlockCaptcha.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockCaptcha : MonoBehaviour
+{
+    [SerializeField] private GameObject blockGO;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private int blockDistance;
+    [SerializeField] private int greenBlockAmt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AddGreens();  
+    }
+    private void AddGreens()
+    {
+        GameObject[] blockList = GameObject.FindGameObjectsWithTag("RedButton");
+
+        int button1 = Random.Range(0, 3);
+        int button2 = Random.Range(3,6);
+        int button3 = Random.Range(6,9);
+
+        CreateGreenButtons(blockList[button1]);
+        CreateGreenButtons(blockList[button2]);
+        CreateGreenButtons(blockList[button3]);
+    }
+    private void CreateGreenButtons(GameObject g)
+    {
+        g.GetComponent<Image>().color = Color.green;
+        g.gameObject.tag = "GreenButton";
+    }
+    public void MakeButtonsGreen(GameObject b)
+    {
+        if(b.tag == "RedButton")
+        {
+            b.GetComponent<Image>().color = Color.green;
+            b.gameObject.tag = "GreenButton";
+            greenBlockAmt++;    
+            if(greenBlockAmt >= 9)
+            {
+                SetUpBlocks();  
+            }
+        }
+        else if(b.tag == "GreenButton")
+        {
+            b.GetComponent<Image>().color = Color.red;
+            b.gameObject.tag = "RedButton";
+            greenBlockAmt--;    
+        }
+    }
+    private void SetUpBlocks()
+    {
+        GameObject[] blockList = GameObject.FindGameObjectsWithTag("GreenButton");
+        foreach(GameObject g in blockList)
+        {
+            g.GetComponent<Image>().color = Color.red;
+            g.gameObject.tag = "RedButton";
+        }
+        greenBlockAmt = 3;
+        AddGreens();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
