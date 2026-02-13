f196095e0265603a383b63dec2af991b03add381
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-02-13 11:23:29 -0600

    sound array added to AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    private static AudioManager instance;
+
+    [SerializeField] private List<ClipAndEnum> _sounds;
+
+    public static AudioManager Instance { get => instance; set => instance = value; }
+    public List<ClipAndEnum> Sounds { get => _sounds; set => _sounds = value; }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
+
+[System.Serializable]
+public struct ClipAndEnum
+{
+    [SerializeField] private string _clipName;
+    [SerializeField] private AudioClip[] _clips;
+
+    public string ClipName { get => _clipName; set => _clipName = value; }
+    public AudioClip[] Clips { get => _clips; set => _clips = value; }
 }
