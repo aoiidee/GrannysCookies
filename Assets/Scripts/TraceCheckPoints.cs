cd26679c62d2cd913053ab3a121fb6ac44d77943
Gabe355 <gandrews1@mail.bradley.edu>
2026-02-19 19:25:33 -0600

    LinetracingCaptcha

---
+++ Assets/Scripts/TraceCheckPoints.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TraceCheckPoints : MonoBehaviour
+{
+    [SerializeField] private GameObject[] checkPoints;
+    private GameObject nextCheckPoint;
+    private int i;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        i = 0;
+        nextCheckPoint = checkPoints[i];    
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "CheckPoint")
+        {
+            if(collision.gameObject == nextCheckPoint)
+            {
+                nextCheckPoint.GetComponent<Image>().color = Color.green;
+                if (i >= 3)
+                {
+                    i = 4;
+                    EndCaptcha();   
+                }
+                else
+                {
+                    i++;
+                    nextCheckPoint = checkPoints[i];
+                }
+            }
+        }
+    }
+    public void ReturnToCheckPoint()
+    {
+        if(i > 0)
+        {
+            transform.position = checkPoints[i - 1].transform.position;
+        }
+    }
+    private void EndCaptcha()
+    {
+        print("End");
+    }
+}
