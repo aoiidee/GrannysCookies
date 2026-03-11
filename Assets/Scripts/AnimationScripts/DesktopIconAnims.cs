99a4e1514704a45ffc24806386d77e7a2a7681c7
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-03-11 18:10:53 -0500

    implemented cosmetic desktop buttons

--- Assets/Scripts/AnimationScripts/DesktopIconAnims.cs
+++ Assets/Scripts/AnimationScripts/DesktopIconAnims.cs
@@ -5,24 +5,34 @@ using UnityEngine.EventSystems;
 public class DesktopIconAnims : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     private Button attachedButton;
+    [SerializeField] private Animator _AnimBase;
+
+    private bool isHovering = false;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        //when clicked down
+        //when clicked
+        _AnimBase.Play("ButtonBasePress");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         //start hovering
+        isHovering = true;
+        _AnimBase.Play("ButtonBaseHighlighted");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         //stop hovering
+        isHovering = false;
+        _AnimBase.Play("ButtonBaseReturnIdle");
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         //when click released
+        if (isHovering) _AnimBase.Play("ButtonBaseRelease"); 
+        else _AnimBase.Play("ButtonBaseIdle");
     }
 }
